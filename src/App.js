639926cc8563cd220186fbb2a865fadd2e714483
azayakaWen <emelie.wensberg@hotmail.com>
2023-02-24 09:30:32 +0100

    Adding project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import AddToDataBase from "./components/AddToDataBase"
+
+function App() {
+  return (
+    <div className="App">
+      <AddToDataBase />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddToDataBase.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AddToDataBase = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Spara</button>
+    </div>
+  )
+}
+
+export default AddToDataBase
